474e8711390fcd0e57350b9a0319b454b2189301
dianasaico20 <diana.saico.est@tecazuay.edu.ec>
2023-01-26 12:57:09 -0500

    Subidax3

--- ExamenPracticoM5A/src/main/resources/recursos/datos.sql
+++ ExamenPracticoM5A/src/main/resources/recursos/datos.sql
@@ -3,3 +3,5 @@ insert into producto(id_codigo,cantidad,descripcion,precio)
  values (1,2,'Manzanas verdes',2);
 insert into producto(id_codigo,cantidad,descripcion,precio)
  values (2,2,'Cereal de chocolate',2);
+ insert into producto(id_codigo,cantidad,descripcion,precio)
+ values (3,4,'Utensillos de cocina',3);
